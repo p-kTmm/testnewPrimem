632b81321878d4d3c9f32982a69eec25616ef0a3
Nguyen Vu Khai Tam <72638887+p-kTmm@users.noreply.github.com>
2024-06-05 16:00:29 +0700

    Update memory.py

--- TLCS/memory.py
+++ TLCS/memory.py
@@ -47,28 +47,41 @@ class SumTree:
         idx = self._retrieve(0, s)
         dataIdx = idx - self.capacity + 1
         return (idx, self.tree[idx], self.data[dataIdx])
-
+        
+    def pop(self):
+        oldest_index = self.write  # Point to the oldest entry
+        idx = oldest_index + self.capacity - 1
+        self.update(idx, 0)  # Set the priority to zero to effectively remove it
+        # Optionally, reset the data point if necessary
+        self.data[oldest_index] = None
 
 class Memory:
     e = 0.01
     a = 0.6
     beta = 0.4
     beta_increment_per_sampling = 0.001
+    absolute_error_upper = 1.0  # Giới hạn lỗi tuyệt đối tối đa
 
     def __init__(self, size_max, size_min):
         self.tree = SumTree(size_max)
         self.size_min = size_min
-
+        self.size_max = size_max 
+        
     def _get_priority(self, error):
-        return (np.abs(error) + self.e) ** self.a
-        # return (1 / (np.abs(error) + self.e)) ** self.a
+        clipped_error = np.minimum(np.abs(error) + self.e, self.absolute_error_upper)  # Giới hạn lỗi
+        return clipped_error ** self.a
+        # return (np.abs(error) + self.e) ** self.a
+
 
     def add_sample(self, sample, error):
         """
         Add a sample into the memory with a given priority
         """
         p = self._get_priority(error)
+        if self.tree.n_entries >= self.size_max:
+            self.tree.pop()  # Loại bỏ mẫu cũ nhất nếu cây đầy
         self.tree.add(p, sample)
+
 
     def get_samples(self, n):
         """
